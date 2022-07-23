cb1c7f3d2d3b6a630a56d4b88f7620d2a929c34a
mariammira <mariamosamamira@gmail.com>
2022-07-23 23:50:43 +0200

    add routes

---
+++ src/search.js
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+function Search() {
+ 
+
+  return (
+    <div className="Search">
+        search component
+        <Link to="/" >
+          Go to HomePage 
+  </Link>
+    </div>);
+}
+
+export default Search;
